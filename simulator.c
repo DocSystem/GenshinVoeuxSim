21f0c61c8763d8472abff36ad1aa6935b1e86a03
DocSystem <42613513+DocSystem@users.noreply.github.com>
2021-10-31 18:12:36 +0100

    Ca y'est il marche ptn

--- simulator.c
+++ simulator.c
@@ -21,10 +21,21 @@ void filterNonFocus4stars() {
     return;
 }
 
+double randomDouble(double min, double max) {
+    return rand() / 32767.0 * (max - min) + min;
+}
+
+int randomInt(int min, int max) {
+    return rand() % (max - min + 1) + min;
+}
+
 int main() {
-    copyArray(all4stars, nonFocus4starsTmp);
+    copyArray(all4stars, nonFocus4starsTmp); // copy all4stars to nonFocus4starsTmp
     filterNonFocus4stars(); // remove focus4stars from all4stars and create nonFocus4stars with the remaining elements
     
+    srand(time(NULL));
+
+    // defining variables
     int count = 0;
     int pity5 = 0;
     int pity4 = 0;
@@ -37,13 +48,55 @@ int main() {
 
     bool continuePull = true;
 
+    char c;
+    double luck;
+
+    // main loop
     while (continuePull) {
         int i;
         for (i = 0; i < 10; i++)
         {
-            printf("%d", i);
+            count++;
+            pity5++;
+            pity4++;
+            luck = randomDouble(0, 10000) / 100;
+            if (luck <= getProba5stars(pity5)) {
+                if (!estGaranti5) {
+                    if (randomInt(1,2) == 1) {
+                        printf("\033[93m%i. %s (5 stars - Focus 50/50) - Pity : %i\033[0m\n", count, focus5stars, pity5);
+                    }
+                    else {
+                        printf("\033[93m%i. %s (5 stars - Non Focus 50/50) - Pity : %i\033[0m\n", count, nonFocus5stars[randomInt(0, sizeof(nonFocus5stars) / sizeof(nonFocus5stars[0]) - 1)], pity5);
+                        estGaranti5 = true;
+                    }
+                }
+                else {
+                    printf("\033[93m%i. %s (5 stars - Garanti) - Pity : %i\033[0m\n", count, focus5stars, pity5);
+                    estGaranti5 = false;
+                }
+                pity5 = 0;
+                count5stars++;
+            } else if (luck <= getProba4stars(pity4)) {
+                if (!estGaranti4) {
+                    if (randomInt(1,2) == 1) {
+                        printf("\033[95m%i. %s (4 stars - Focus 50/50) - Pity : %i\033[0m\n", count, focus4stars[randomInt(0, sizeof(focus4stars) / sizeof(focus4stars[0]) - 1)], pity5);
+                    }
+                    else {
+                        printf("\033[95m%i. %s (4 stars - Non Focus 50/50) - Pity : %i\033[0m\n", count, nonFocus4stars[randomInt(0, sizeof(nonFocus4stars) / sizeof(nonFocus4stars[0]) - 1)], pity5);
+                        estGaranti4 = true;
+                    }
+                }
+                else {
+                    printf("\033[95m%i. %s (4 stars - Garanti) - Pity : %i\033[0m\n", count, focus4stars[randomInt(0, sizeof(focus4stars) / sizeof(focus4stars[0]) - 1)], pity5);
+                    estGaranti4 = false;
+                }
+                pity4 = 0;
+                count4stars++;
+            } else {
+                printf("%i. 3 stars - Pity : %i\n", count, pity5);
+            }
         }
-        break;
+        scanf("%c", &c);
     }
     return 0;
 }
